f2fcfa176e6ee161629f6811f5515db99b77d47d
Brian <briancmuhammad@yahoo.com>
2024-09-23 14:55:53 -0700

    Finished basic structure for side navigation bar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,12 +11,13 @@ import Image from "next/image"
 
 export default function Sidebar() {
   return (
-    <div className="hidden sm:flex flex-col fixed">
-      <div className="py-3">
+    <div className="h-full hidden sm:flex flex-col fixed xl:ml-24">
+     
+      <nav className="h-full relative xl:space-y-1.5">
+      <div className="flex justify-center items-center xl:justify-start py-3 xl:p-3">
         <Image src={"/assets/twitter-logo.png"} width={34} height={34}/>
 
       </div>
-      <nav>
         <SidebarLink Icon={HomeIcon} text={"Home"} />
         <SidebarLink Icon={HashtagIcon} text={"Explore"} />
         <SidebarLink Icon={BellIcon} text={"Notifications"} />
@@ -24,16 +25,16 @@ export default function Sidebar() {
         <SidebarLink Icon={BookmarkIcon} text={"Home"} />
         <SidebarLink Icon={UserIcon} text={"Profile"} />
         <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"} />
- 
+        <div className="absolute bottom-0">User</div>
       </nav>
-      <div>User</div>
+      
     </div>
   );
 }
 
 function SidebarLink({ text, Icon }) {
   return (
-    <li className="flex mb-6 items-center text-xl space-x-3">
+    <li className="hoverAnimation flex mb-3 xl:justify-start justify-center items-center text-xl space-x-3">
       <Icon className="h-7" />
       <span className="hidden xl:inline" >
       {text}
