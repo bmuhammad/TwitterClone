0bb4bd8450797f1e07368c5480dce46edc7976f7
Brian <briancmuhammad@yahoo.com>
2024-09-24 07:49:07 -0700

    added structure for banner on the bottom

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,17 @@ import localFont from "next/font/local";
 import Sidebar from "@/components/Sidebar";
 import Postfeed from "@/components/Postfeed"
 import Trending from "@/components/Trending"
+import BottomBanner from "@/components/BottomBanner";
 
 export default function Home() {
   return (
+    <div>
     <div className="bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto flex">
    <Sidebar /> 
     <Postfeed /> 
     <Trending />
     </ div>
+    <BottomBanner />
+    </div>
   );
 }
