75b014ed48db350adcb5e319ff5782fdf4ee02a1
Brian <briancmuhammad@yahoo.com>
2024-09-23 17:49:07 -0700

    Added/Finished Trending section

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,17 +1,21 @@
-import { ChartBarIcon, ChatIcon, HeartIcon, UploadIcon } from "@heroicons/react/outline";
+import {
+  ChartBarIcon,
+  ChatIcon,
+  HeartIcon,
+  UploadIcon,
+} from "@heroicons/react/outline";
 
 export default function Tweet() {
   return (
     <div className="border-b border-gray-700">
       <TweetHeader />
       <div className="p-3 ml-16 text-gray-500 flex space-x-14">
-      <ChatIcon className="w-5 cursor-pointer hover:text-green-400" />
-      <HeartIcon className="w-5 cursor-pointer hover:text-pink-500" />
-      <ChartBarIcon className="w-5 cursor-not-allowed" />
-      <UploadIcon className="w-5 cursor-not-allowed" />
+        <ChatIcon className="w-5 cursor-pointer hover:text-green-400" />
+        <HeartIcon className="w-5 cursor-pointer hover:text-pink-500" />
+        <ChartBarIcon className="w-5 cursor-not-allowed" />
+        <UploadIcon className="w-5 cursor-not-allowed" />
       </div>
     </div>
-  
   );
 }
 
@@ -23,13 +27,12 @@ export function TweetHeader() {
         src="/assets/kylie.png"
       />
       <div>
-<div className="text-gray-500 flex items-center space-x-2 mb-1">
-  <span>@kylie</span>
-  <div className="w-1 h-1 bg-gray-500 rounded-full "></div>
-  <span>2 hours ago</span>
-</div>
-<span>Text</span>
-
+        <div className="text-gray-500 flex items-center space-x-2 mb-1">
+          <span>@kylie</span>
+          <div className="w-1 h-1 bg-gray-500 rounded-full "></div>
+          <span>2 hours ago</span>
+        </div>
+        <span>Text</span>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ import Trending from "@/components/Trending"
 export default function Home() {
   return (
     <div className="bg-black min-h-screen text-[#E7E9EA] max-w-[1400px] mx-auto flex">
-    <Sidebar />
-    <Postfeed />
+   <Sidebar /> 
+    <Postfeed /> 
     <Trending />
     </ div>
   );
